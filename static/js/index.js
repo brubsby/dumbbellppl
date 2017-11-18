1cdc595773cab13561aebb74827d7d65edb83640
Tyler Busby <TylerBusby333@gmail.com>
2017-11-18 00:46:25 -0600

    Add timer audio and reset button

--- static/js/index.js
+++ static/js/index.js
@@ -12,7 +12,15 @@ function startTimer()
         $('.progress-bar').css('width', (100*secondsRemaining/seconds)+'%');
     } else {
         clearInterval(counterBack);
+        document.getElementById('alert-audio').play();
     }
 
     }, 1000);
+}
+
+function resetTimer()
+{
+    clearInterval(counterBack);
+    $('.progress-bar').css('width', '100%');
+    document.getElementById('alert-audio').pause();
 }
